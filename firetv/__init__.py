c405dbc0de56f5c6dd619ae872bcbd44f35a48a6
David Burns <david@burns.cc>
2015-11-01 02:31:08 +0000

    Change so that apps arnt on unless firetv is playing

--- firetv/__init__.py
+++ firetv/__init__.py
@@ -86,6 +86,8 @@ class FireTV:
 
     def app_state(self, app):
         """ Informs if application is running """
+        if self.state != STATE_PLAYING:
+            return STATE_OFF
         if len(self._ps(app)) > 0:
             return STATE_ON
         return STATE_OFF
