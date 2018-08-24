14ada6d012001db4cdf37e393741f42a1702bfd2
Jeff Irion <jefflirion@users.noreply.github.com>
2018-08-24 16:37:13 -0700

    Bump version to 1.0.5.1 and fix requirements

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='firetv',
-    version='1.0.5.dev',
+    version='1.0.5.1',
     description='Communicate with an Amazon Fire TV device via ADB over a network.',
     url='https://github.com/happyleavesaoc/python-firetv/',
     license='MIT',
     author='happyleaves',
     author_email='happyleaves.tfr@gmail.com',
     packages=['firetv'],
-    install_requires=['adb==1.3.0.dev'],
+    install_requires=['pycryptodome', 'rsa', 'adb>=1.3.0'],
     extras_require={
         'firetv-server': ['Flask>=0.10.1', 'PyYAML>=3.12']
     },
